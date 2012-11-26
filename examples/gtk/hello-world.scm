f4ea48219977a2e7ddfb89428dd2b7b3f4e541de
Sergey Cherepanov <s.g.cherepanov@gmail.com>
2012-11-26 11:00:03 +1100

    Update examples/gtk/hello-world.scm.

--- examples/gtk/hello-world.scm
+++ examples/gtk/hello-world.scm
@@ -3,4 +3,7 @@
 (require 'gtk.la)
 
 (gtk-init (command-line-args))
+(define window (gtk-window-new 'gtk-window-toplevel))
+(gtk-window-set-title window "Hello, World!")
+(gtk-widget-show-all window)
 (gtk-main)
