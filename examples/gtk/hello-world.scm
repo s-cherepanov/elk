7045710d4568fe942ce61329773646f1e003d6db
Sergey Cherepanov <s.g.cherepanov@gmail.com>
2012-12-04 12:33:42 +1100

    Update hello-world.scm.

--- examples/gtk/hello-world.scm
+++ examples/gtk/hello-world.scm
@@ -5,5 +5,7 @@
 (gtk-init (command-line-args))
 (define window (gtk-window-new 'gtk-window-toplevel))
 (gtk-window-set-title window "Hello, World!")
+(define callback (lambda args (gtk-main-quit)))
+(g-signal-connect window "destroy" callback)
 (gtk-widget-show-all window)
 (gtk-main)
